119d581db8bbbe4cc73b76004c778189e88efafd
vishaalkaran <35380300+vishaalkaran@users.noreply.github.com>
2018-01-31 17:52:48 -0700

    update

--- Jan2018DemoWebsite/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Jan2018DemoWebsite.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Jan2018DemoWebsite/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jan2018DemoWebsite.Global" Language="C#" %>
