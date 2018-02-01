78192f5c97c62aa6b50c0633699fe8b80b3c7a04
vishaalkaran <35380300+vishaalkaran@users.noreply.github.com>
2018-01-31 17:37:17 -0700

    update

--- ChinookSystem/BLL/AlbumController.cs
+++ ChinookSystem/BLL/AlbumController.cs
@@ -4,9 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
 namespace ChinookSystem.BLL
 {
+    [DataObject]
     class AlbumController
     {
+        //list
+        public List<Album> Album_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        //get/play
+        public Album Album_Get(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+
+        //add
+        public int Album_Add(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                context.Albums.Add(item);
+                context.SaveChanges();
+                return item.AlbumId;
+            }
+        }
+
+        //update
+        public int Album_Update(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                return context.SaveChanges();
+            }
+        }
+        //delete 
     }
 }
--- ChinookSystem/BLL/TrackController.cs
+++ ChinookSystem/BLL/TrackController.cs
@@ -4,8 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
 namespace ChinookSystem.BLL
 {
+    [DataObject]
     class TrackController
     {
     }
